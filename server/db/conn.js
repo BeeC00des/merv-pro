69f41014664c0891d964f9d48d46705589f29dd1
BeeC00des <ajayibcd@gmail.com>
2023-04-10 10:28:20 +0000

    fixing bug

--- server/db/conn.js
+++ server/db/conn.js
@@ -8,12 +8,12 @@ const client =new MongoClient(Db, {useNewUrlParser: true,useUnifiedTopology: tru
 var _db;
  
 module.exports = {
-  connectToServer: function (callback) {
+  connectToServer: function (callback) { 
     client.connect(function (err, db) {
       // Verify we got a good "db" object
-      if (db)
-      {
+      if (db){
         _db = db.db("employees");
+      
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
--- server/routes/record.js
+++ server/routes/record.js
@@ -7,6 +7,7 @@ const recordRoutes = express.Router();
  
 // This will help us connect to the database
 const dbo = require("../db/conn");
+// console.log(dbo);
  
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
